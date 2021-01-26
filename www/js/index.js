a2ac6226d89872c033e46059fef832f83cc6c74e
Twilii <gola1_20@uni.worc.ac.uk>
2021-01-26 11:11:02 +0000

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,10 @@
-Backendless.initApp("A721C3AE-F98A-1BA0-FF1E-FB1227062A00", "61ECF7F2-6AE1-4114-8EDE-FEC2F39EB6BF");
 
+Backendless.initApp('A721C3AE-F98A-1BA0-FF1E-FB1227062A00','61ECF7F2-6AE1-4114-8EDE-FEC2F39EB6BF');
+
+//test for page ready
 $(document).on("pageshow","#todopage", onPageShow);
+//test for adding a new task
+$(document).on("click", "#addTaskButton", onAddTask);
 
 //when page is loaded
 function onPageShow() {
@@ -12,9 +16,40 @@ function onPageShow() {
 Backendless.Data.of("Tasks").find().then(processResults).catch(error);
 
 function processResults(tasks) {
-    alert(tasks[0].Task)
+    //empty placeholder tasks
+    $('#taskList').empty();
+    //loop through until no more tasks in DB
+    for (var i=0; i < tasks.length; i++) {
+	$('#taskList').append("<li>"+tasks[i].Task+"</li>");
+    }
+    //refresh to update
+    $('#taskList').listview('refresh');
 }
 
 function error(err) {
+    //show err if failed to retrieve
     alert(err);
+}
+
+
+
+function onAddTask() {
+    console.log('Button pressed');
+    //assign variables
+    var taskText = $('#addTaskText').val();
+    
+    //Create object and assign task attribute of object to add input
+    var newTask = { };
+    newTask.Task = taskText;
+    
+    Backendless.Data.of("Tasks").save(newTask).then(saved).catch(error);
+    
+}
+
+function saved(savedTask) {
+    console.log("saved " + savedTask.Task);
+    //processResults();
+    $('#addTaskText').val("");
+    $('#taskList').append("<li>"+savedTask.Task+"</li>");
+    $('#taskList').listview('refresh');
 }
